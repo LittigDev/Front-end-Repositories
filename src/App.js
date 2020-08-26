e3c05657e77849d75af7b2ec6ce6fdddc800a2fd
Gustavo Littig Albuquerque <gustavo.l@infolog.com.br>
2020-08-26 15:56:46 -0300

    Subindo desafio concluído

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    await api.post('repositories', {
+      title:"Postgres Pessoal",
+      url:"https://google.com.br",
+      techs:"React"
+    }).then(response => {
+      setRepositories([...repositories, response.data])
+    })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          { 
+            repositories.map( (item, id) => 
+              <li key={item.id} >
+                {item.title}
+                <button onClick={() => handleRemoveRepository(item.id)} key={id}>
+                  Remover
+                </button>
+              </li>
+            )
+          }
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
